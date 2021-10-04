e8e0eeae1223e3411be4c9459e04f04b3c7b8121
brenno-calado <brenno.calado@gmail.com>
2021-10-04 11:38:18 -0300

    filter interRegional Routes and map routes Regions

--- regionRouter.js
+++ regionRouter.js
@@ -8,7 +8,10 @@ const findRegionRoute = (origin, destination) => {
 
   if (regionOfDestination === regionOfOrigin) return routesAmount;
 
-  console.log(routes);
+  const interRegionalRoutes = routes.filter((route) => capitals[route[0]].region !== capitals[route[1]].region );
+  const regionRoutes = interRegionalRoutes.map((route) => [capitals[route[0]].region, capitals[route[1]].region])
+  console.log(regionRoutes);
+  
 }
 
 findRegionRoute('Macapá', 'Recife');
